c6b876481a3af885d09e50588fb6f0ff62eb1e5b
Christina <huleechristina@gmail.com>
2020-06-24 16:28:10 -0400

    add card-list component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { CardList } from './components/card-list/card-list.component.jsx'
 
 class App extends Component {
   constructor () {
     super();
     
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-        },
-        {
-          name: "Dracula",
-        },
-        {
-          name: "Zombie",
-        }
-      ]
+      monsters: []
     };
   } 
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
 
   render () {
     return (
       <div className="App">
-      {
-        this.state.monsters.map( (monster, index) => ( 
-          <h1 key={index}>{ monster.name }</h1> 
-        ))
-      }
+        <CardList monsters={this.state.monsters} />
       </div>
     )
   }
 
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
